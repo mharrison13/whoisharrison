796aa9da136bfe306e6f146e055ea3425e8d8f95
Michael Harrison <mharrison13@cnm.edu>
2017-04-26 16:49:19 -0600

    project updated with arguments, goals, persona and use-case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,12 +4,12 @@
 </head>
 <body>
 <h1>Milestone 1 PWP</h1>
-	<h2><em>Purpose, Audience & Goals</em></h2><br>
-	<h2>Purpose</h2><br>
+	<h2><em>Purpose, Audience & Goals</em></h2>
+	<h2>Purpose</h2>
 	<p>I would like for my PWP to build a new online portfolio.</p>
-	<h2>Audience</h2><br>
+	<h2>Audience</h2>
 	<p>My audience will be potential clients. The site will be desiged as a clean, simple way for my audience to see my work and contact me for potential work.</p>
-	<h2>Goals</h2><br>
+	<h2>Goals</h2>
 	<ol>
 		<li>Acquire freelance projects</li>
 		<li>Network with other professional</li>
@@ -17,8 +17,8 @@
 		<li>Make money!</li>
 	</ol>
 
-	<h2><em>Persona</em></h2><br>
-	<p><strong>Name:</strong> Bob L. Business-Owner<br>
+	<h2><em>Persona</em></h2>
+	<p><strong>Name:</strong> Bob L. Business-Owner
 		<strong>Age:</strong> 50</p>
 	<p><strong>Profession: </strong> Employer</p>
 	<p><Strong>Technology: </Strong> Windows, Mac and/or mobile user</p>
@@ -30,9 +30,5 @@
 		<p>Last week, Bob L. Business-Owner was speaking a friend over his lack of business. His friend took a look at his website and realised that Bob's current website failed to showcase his product. On top of that, there was no way for his customers to find him or contact him. Bob's friend, having a background in marketing, suggested that rather that Bob trying to save money by doing his own website, he hire a professional to build a custom site that reflects the high quality product that Bob sells.</p>
 		<p>Bob swears by his Mac and iPhone and believe that with these to components, he can build his own website. He just realized that an effective web presence is harder than he originally though.</p>
 		<p></p>
-
-
-
-
 
 </body>
