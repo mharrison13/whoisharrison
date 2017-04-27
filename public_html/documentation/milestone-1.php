feca6fda0ae0db5cd1f9023932dd0b3a93b89683
Michael Harrison <mharrison13@cnm.edu>
2017-04-27 17:01:35 -0600

    cleaned up

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,34 +1,48 @@
-<head>
-	<title>Whois Harrison</title>
-	<style></style>
-</head>
-<body>
-<h1>Milestone 1 PWP</h1>
-	<h2><em>Purpose, Audience & Goals</em></h2>
-	<h2>Purpose</h2>
-	<p>I would like for my PWP to build a new online portfolio.</p>
-	<h2>Audience</h2>
-	<p>My audience will be potential clients. The site will be designed as a clean, simple way for my audience to see my work and contact me for potential work.</p>
-	<h2>Goals</h2>
-	<ol>
-		<li>Acquire freelance projects</li>
-		<li>Network with other professional</li>
-		<li>Provide means for contact</li>
-		<li>Make money!</li>
-	</ol>
+<!DOCTYPE html>
+<html lang="en">
 
-	<h2><em>Persona</em></h2>
-	<p><strong>Name:</strong> Bob L. Business-Owner
-	<p><strong>Age:</strong> 50</p>
-	<p><strong>Profession: </strong> Employer</p>
-	<p><Strong>Technology: </Strong> Windows, Mac and/or mobile user</p>
-	<p><strong>Attitudes and Behavior: </strong>Mr Business-Owner, owner of local business is looking for a web developer / DBA for their local business, whatever that may be.</p>
-	<p><strong>Frustrations and Needs: </strong> Bob is looking for a talented, hardworking and knowledgeable employee to help advance his business and build his web presence above that of his competition.</p>
-	<p><strong>Goals: </strong>Bob really needs a web site that solves the problem of his potential customers finding and purchasing from him. He built his own website on wix and now realizes that he is losing business to his competitors because his website looks unprofessional.</p><br>
+	<head>
+		<meta charset="utf-8">
 
-	<h2><em>Use Case</em></h2>
-		<p>Last week, Bob L. Business-Owner was speaking a friend over his lack of business. His friend took a look at his website and realised that Bob's current website failed to showcase his product. On top of that, there was no way for his customers to find him or contact him. Bob's friend, having a background in marketing, suggested that rather that Bob trying to save money by doing his own website, he hire a professional to build a custom site that reflects the high quality product that Bob sells.</p>
-		<p>Bob swears by his Mac and iPhone and believe that with these to components, he can build his own website. He just realized that an effective web presence is harder than he originally though.</p>
-		<p></p>
+		<title>Whois Harrison</title>
+		<style></style>
+	</head>
+	<body>
+	<h1>Milestone 1 PWP</h1>
+		<h2><em>Purpose, Audience & Goals</em></h2>
+		<h2>Purpose</h2>
+		<p>I would like for my PWP to build a new online portfolio.</p>
+		<h2>Audience</h2>
+		<p>My audience will be potential clients. The site will be designed as a clean, simple way for my audience to see my work and contact me for potential work.</p>
+		<h2>Goals</h2>
+		<ol>
+			<li>Acquire freelance projects</li>
+			<li>Network with other professional</li>
+			<li>Provide means for contact</li>
+			<li>Make money!</li>
+		</ol>
 
+		<h2><em>Persona</em></h2>
+		<p><strong>Name:</strong> Bob L. Business-Owner
+		<p><strong>Age:</strong> 50</p>
+		<p><strong>Profession: </strong> Employer</p>
+		<p><Strong>Technology: </Strong> Windows, Mac and/or mobile user</p>
+		<p><strong>Attitudes and Behavior: </strong>Mr Business-Owner, owner of local business is looking for a web developer / DBA for their local business, whatever that may be.</p>
+		<p><strong>Frustrations and Needs: </strong> Bob is looking for a talented, hardworking and knowledgeable employee to help advance his business and build his web presence above that of his competition.</p>
+		<p><strong>Goals: </strong>Bob really needs a web site that solves the problem of his potential customers finding and purchasing from him. He built his own website on wix and now realizes that he is losing business to his competitors because his website looks unprofessional.</p><br>
+
+		<h2><em>Use Case</em></h2>
+			<p>Last week, Bob L. Business-Owner was speaking a friend over his lack of business. His friend took a look at his website and realised that Bob's current website failed to showcase his product. On top of that, there was no way for his customers to find him or contact him. Bob's friend, having a background in marketing, suggested that rather that Bob trying to save money by doing his own website, he hire a professional to build a custom site that reflects the high quality product that Bob sells.</p>
+			<p>Bob swears by his Mac and iPhone and believe that with these to components, he can build his own website. He just realized that an effective web presence is harder than he originally though.</p>
+			<p>Whoisharrison.com will deliver all the info necessary information and samples for Bob L. Business owner to see samples of Michael Harrison's work. The brand will be based on a clean and easy UX that will lead Bob directly to the request info submit form. Within 10 seconds, Bob will immediately see a high quality protfolio site. Withing 5 minutes of viewing Whoisharrison.com Bob will be interested in the work and will want to reach out for more information</p>
+
+		<h2>Interaction Flow</h2>
+		<ol>
+			<li>User comes to site</li>
+			<li>Whoisharrison.com delivers portfolio info, samples of work & contact info</li>
+			<li>User scrolls down through site</li>
+			<li>Whoisharrison.com offers a contact submit form</li>
+			<li>User enters information in <em>request info</em> contact form.</li>
+			<li>Whoisharrison.com submits info to Michael Harrison's personal email</li>
+		</ol>
 </body>
