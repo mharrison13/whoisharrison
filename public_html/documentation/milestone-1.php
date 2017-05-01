609bd0f0a6563d9a296f90ff2cf5b69bfa312384
Michael Harrison <mharrison13@cnm.edu>
2017-05-01 11:56:28 -0600

    touch-ups

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -27,15 +27,14 @@
 		<p><strong>Name:</strong> Bob L. Business-Owner
 		<p><strong>Age:</strong> 50</p>
 		<p><strong>Profession: </strong> Employer</p>
-		<p><Strong>Technology: </Strong> Windows, Mac and/or mobile user</p>
-		<p><strong>Attitudes and Behavior: </strong>Mr Business-Owner, owner of local business is looking for a web developer / DBA for their local business, whatever that may be.</p>
-		<p><strong>Frustrations and Needs: </strong> Bob is looking for a talented, hardworking and knowledgeable employee to help advance his business and build his web presence above that of his competition.</p>
+		<p><Strong>Technology: </Strong> At work, Mac Pro 13inch. When at home Mac user with a cinema display. Also uses iPhone7.</p>
+		<p><strong>Attitudes and Behavior: </strong>Mr Business-Owner, owner of local business is looking for a web developer / DBA for their local business, whatever that may be. Last week, Bob L. Business-Owner was speaking to a friend over his lack of business. His friend looked at his website and realised that Bob's current website failed to showcase his product. On top of that, there was no way for his customers to find him or contact him. Bob's friend, having a background in marketing, suggested that rather that Bob trying to save money by doing his own website, he hire a professional to build a custom site that reflects the high quality product that Bob sells.</p>
+		<p><strong>Frustrations and Needs: </strong> Bob needs to hire a talented, hardworking and knowledgeable employee to help advance his business and build his web presence above that of his competition.</p>
 		<p><strong>Goals: </strong>Bob really needs a web site that solves the problem of his potential customers finding and purchasing from him. He built his own website on wix and now realizes that he is losing business to his competitors because his website looks unprofessional.</p><br>
 
 		<h2>Use Case</h2>
-			<p>Last week, Bob L. Business-Owner was speaking to a friend over his lack of business. His friend looked at his website and realised that Bob's current website failed to showcase his product. On top of that, there was no way for his customers to find him or contact him. Bob's friend, having a background in marketing, suggested that rather that Bob trying to save money by doing his own website, he hire a professional to build a custom site that reflects the high quality product that Bob sells.</p>
-			<p>Bob swears by his Mac and iPhone and believe that with these two components, he can build his own website. He just recently realized that an effective web presence is harder than he originally thought.</p>
-		<p><a href="http://www.whoisharrison.com">Whoisharrison.com</a> will deliver necessary information and samples for Bob L. Business owner to see samples of Michael Harrison's work. Whoisharrison.com brand will be based on a clean and easy UX that will lead Bob directly to the request info submit form with a anchor style nav at the top of the page which will allow for Bob to jump around as needed. Within 10 seconds, Bob will immediately see a high quality portfolio site. Within 5 minutes of viewing Whoisharrison.com, Bob will be interested in the work and will want to reach out for more information.</p><br>
+			<p>Feeling pressure to complete his website, Bob L. Business-Owner has taken the time to research local web developers in the area. He has narrowed his search down to three candidates including myself. </p>
+			<p>It is currently 11:30AM on Monday and Bob has decided to look over my website during his lunch hour with the intent of setting up a interviews. He has given himself a tight deadline to review the top candidate websites and mine if first up.</p>
 
 		<h2>Interaction Flow </h2>
 		<ol>
@@ -46,4 +45,5 @@
 			<li>User enters information in <em>request info</em> contact form.</li>
 			<li>Whoisharrison.com submits info to Michael Harrison's personal email</li>
 		</ol>
+		<p><a href="http://www.whoisharrison.com">Whoisharrison.com</a> will deliver necessary information and samples for Bob L. Business owner to see samples of Michael Harrison's work. Whoisharrison.com brand will be based on a clean and easy UX that will lead Bob directly to the request info submit form with a anchor style nav at the top of the page which will allow for Bob to jump around as needed. Within 10 seconds, Bob will immediately see a high quality portfolio site. Within 5 minutes of viewing Whoisharrison.com, Bob will be interested in the work and will want to reach out for more information.</p><br>
 </body>
