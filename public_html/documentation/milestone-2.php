1d2a8ca3d54125cb36ea658d23b919b16190bbfb
Michael Harrison <mharrison13@cnm.edu>
2017-05-14 22:41:46 -0600

    created milestone-2.php

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>WhoisHarrison Milestone 2</title>
+	</head>
+	<body>
+		<h1>Phase 2α Requirements</h1>
+		<p>This website will serve as a portfolio of my work with it's primary focus being programming with the following skills:</p>
+		<ol>
+			<li>Lamp</li>
+			<li>CSS</li>
+			<li>HTML5</li>
+			<li>JavaScript</li>
+			<li>JSON & Ajax</li>
+			<li>jQuery</li>
+			<li>PHP</li>
+			<li>mySQL</li>
+			<li>Angular 4</li>
+		</ol>
+		<p>The type of content I plan to feature on my site will be screen shots and links of projects I have completed during our bootcamp. I will also include several websites I have built in the past with WordPress, Drupal & HTML. I will also include links to my github and linkedin accounts.</p>
+		<p>My strategy will be to demonstrate my skills through a visually appealing, <em>technical feeling</em> but clean website that is easy to navigate. This site will have clear descriptions of my skills and my resume. Finally, any potential employer will be able to immediately identify me as a qualified employee in the tech field. </p>
+	</body>
