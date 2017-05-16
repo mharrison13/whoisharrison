babd2b4c75b8c8e4e3daf998a4a1754d2ef47baf
Michael Harrison <mharrison13@cnm.edu>
2017-05-16 10:10:59 -0600

    created milestone-2.php cleaned up @gkephart

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -4,8 +4,10 @@
 		<title>WhoisHarrison Milestone 2</title>
 	</head>
 	<body>
-		<header>PWP milestone 2a</header>
-		<h1>Phase 2α Requirements</h1>
+		<header>
+			<h1>Phase 2α Requirements</h1>
+		</header>
+
 		<p>This website will serve as a portfolio of my work with it's primary focus being programming with the following skills:</p>
 		<ol>
 			<li>Lamp</li>
@@ -20,8 +22,8 @@
 		</ol>
 		<p>The type of content I plan to feature on my site will be screen shots and links of projects I have completed during our bootcamp. I will also include several websites I have built in the past with WordPress, Drupal & HTML. I will also include links to my github and linkedin accounts.</p>
 		<p>My strategy will be to demonstrate my skills through a visually appealing, <em>technical feeling</em> but clean website that is easy to navigate. This site will have clear descriptions of my skills and my resume. Finally, any potential employer will be able to immediately identify me as a qualified employee in the tech field. </p>
-		<img src="mobile-wireframe-1.png">
-		<img src="mobile-wireframe-2.jpg">
-		<img src="website-wireframe.png">
+		<img src="../images/mobile-wireframe-1.png">
+		<img src="../images/mobile-wireframe-2.jpg">
+		<img src="../images/website-wireframe.png">
 	</body>
 </html>
