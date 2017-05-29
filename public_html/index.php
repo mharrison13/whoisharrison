143218b7573480a5ea25235cca0a96fe25d395c0
Michael Harrison <mharrison13@cnm.edu>
2017-05-29 00:26:15 -0600

    whois harrison mobile first

---
+++ public_html/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- My Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Whois Harrison</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<header>
+				<nav class="navbar navbar-default navbar-fixed-bottom">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Michael Harrison</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="main-menu">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="https://github.com/mharrison13" target="_blank">GitHub</a></li>
+								<li><a href="/resume.html" target="_blank">Resume</a></li>
+								<li><a href="#portfolio" target="_blank">Portfolio</a></li>
+								<li><a href="#contact" target="_blank">Contact</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+
+			<!-- My Welcome Section -->
+			<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="whois">
+								<h1>Whois Harrison?</h1>
+								<p>intro copy</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Photo and Text Section -->
+			<section class="bg-info">
+				<div class="container">
+					<p>Slider to come here</p>
+				</div>
+			</section>
+
+			<!-- Thumbnail Section -->
+			<section>
+				<div class="container">
+					<div class="row">
+						<h3>Contact</h3>
+						<p>This will be my contact form area</p>
+					</div><!--/.row-->
+				</div><!--/.container-->
+			</section>
+
+		</div>
+		<footer class="bg-info text-center">
+			<div class="container">
+				<div class="row">
+					<div class="footer">
+						Whois Harrison © 2017 | github | resume | portfolio | contact
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
