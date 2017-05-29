2cc0efd504d8f9945cba3bd930c97db53dc1eae9
Michael Harrison <mharrison13@cnm.edu>
2017-05-29 01:40:35 -0600

    added contact

--- public_html/index.php
+++ public_html/index.php
@@ -30,6 +30,17 @@
 		<!-- jQuery v3.0 -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 
+		<!-- jQuery Form, Additional Methods, Validate for contact-->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
 		<!-- minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
@@ -127,8 +138,143 @@
 				</div>
 			</section>
 
-			<!-- Thumbnail Section -->
+			<!-- Contact Section -->
 			<section>
+<?php
+/**
+ * require all composer dependencies; requiring the autoload file loads all composer packages at once
+ **/
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+/**
+ * require mailer-config.php
+ **/
+require_once("mail-config.php");
+
+// verify user's reCAPTCHA input
+$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+$resp = $recaptcha->verify($_POST["g-recaptcha-response"], $_SERVER["REMOTE_ADDR"]);
+
+try {
+
+	// if reCAPTCHA error, output the error code to the user
+	if (!$resp->isSuccess()) {
+		throw(new Exception("reCAPTCHA error!"));
+	}
+
+	// sanitize the inputs from the form: name, email, subject, and message
+	// this assumes jQuery (not Angular will be submitting the form, so we're using the $_POST superglobal
+	$name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+	$subject = filter_input(INPUT_POST, "subject", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$message = filter_input(INPUT_POST, "message", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	// create Swift message
+	$swiftMessage = Swift_Message::newInstance();
+
+	// attach the sender to the message
+	// this takes the form of an associative array where the Email is the key for the real name
+	$swiftMessage->setFrom([$email => $name]);
+
+	/**
+	 * attach the recipients to the message
+	 * $MAIL_RECIPIENTS is set in mail-config.php
+	 **/
+	$recipients = $MAIL_RECIPIENTS;
+	$swiftMessage->setTo($recipients);
+
+	// attach the subject line to the message
+	$swiftMessage->setSubject($subject);
+
+	/**
+	 * attach the actual message to the message
+	 * here, we set two versions of the message: the HTML formatted message and a special filter_var()ed
+	 * version of the message that generates a plain text version of the HTML content
+	 * notice one tactic used is to display the entire $confirmLink to plain text; this lets users
+	 * who aren't viewing HTML content in Emails still access your links
+	 **/
+	$swiftMessage->setBody($message, "text/html");
+	$swiftMessage->addPart(html_entity_decode($message), "text/plain");
+
+	/**
+	 * send the Email via SMTP; the SMTP server here is configured to relay everything upstream via CNM
+	 * this default may or may not be available on all web hosts; consult their documentation/support for details
+	 * SwiftMailer supports many different transport methods; SMTP was chosen because it's the most compatible and has the best error handling
+	 * @see http://swiftmailer.org/docs/sending.html Sending Messages - Documentation - SwitftMailer
+	 **/
+	$smtp = Swift_SmtpTransport::newInstance("localhost", 25);
+	$mailer = Swift_Mailer::newInstance($smtp);
+	$numSent = $mailer->send($swiftMessage, $failedRecipients);
+
+	/**
+	 * the send method returns the number of recipients that accepted the Email
+	 * so, if the number attempted is not the number accepted, this is an Exception
+	 **/
+	if($numSent !== count($recipients)) {
+		// the $failedRecipients parameter passed in the send() method now contains contains an array of the Emails that failed
+		throw(new RuntimeException("unable to send email"));
+	}
+
+	// report a successful send
+	echo "<div class=\"alert alert-success\" role=\"alert\">Email successfully sent.</div>";
+
+} catch(Exception $exception) {
+	echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to send email: " . $exception->getMessage() . "</div>";
+}
+?>
+				<!--Begin Contact Form-->
+				<form id="contact-form" action="php/mailer.php" method="post">
+					<div class="form-group">
+						<label for="name">Name <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-user" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email">Email <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+							<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="subject">Subject</label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-pencil" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message">Message <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-comment" aria-hidden="true"></i>
+							</div>
+							<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+						</div>
+					</div>
+
+					<!-- reCAPTCHA -->
+					<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+					<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+				</form>
+
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
+
 				<div class="container">
 					<div class="row">
 						<h3>Contact</h3>
