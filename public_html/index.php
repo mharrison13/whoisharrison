c0ec243c25ef5baddcb8b2b85ae3cc017ddb3a82
Michael Harrison <mharrison13@cnm.edu>
2017-05-30 14:57:16 -0600

    added client logos

--- public_html/index.php
+++ public_html/index.php
@@ -2,21 +2,23 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 		<!-- Font Awesome -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 		<!-- My Custom CSS -->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
-		<link rel="stylesheet" href="css/jquery.fancybox.min.css" type="text/css" >
+		<link rel="stylesheet" href="css/jquery.fancybox.min.css" type="text/css">
 		<link rel="stylesheet" href="css/grid.css" type="text/css">
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -32,9 +34,12 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 
 		<!-- jQuery Form, Additional Methods, Validate for contact-->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
 
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
@@ -43,7 +48,9 @@
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 
 		<!-- minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
 
 		<title>Whois Harrison</title>
 	</head>
@@ -55,7 +62,8 @@
 					<div class="container">
 						<!-- Brand and toggle get grouped for better mobile display -->
 						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+									  data-target="#main-menu" aria-expanded="false">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
@@ -67,10 +75,15 @@
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="main-menu">
 							<ul class="nav navbar-nav navbar-right">
-								<li><a href="https://github.com/mharrison13" target="_blank"><i class="fa fa-github" aria-hidden="true"></i> GitHub</a></li>
-								<li><a href="/resume.html" target="_blank"><i class="fa fa-file-text" aria-hidden="true"></i> Resume</a></li>
-								<li><a href="#portfolio" target="_blank"><i class="fa fa-laptop" aria-hidden="true"></i> Portfolio</a></li>
-								<li><a href="#contact" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i> Contact</a></li>
+								<li><a href="https://github.com/mharrison13" target="_blank"><i class="fa fa-github"
+																													 aria-hidden="true"></i>
+										GitHub</a></li>
+								<li><a href="/resume.html" target="_blank"><i class="fa fa-file-text" aria-hidden="true"></i>
+										Resume</a></li>
+								<li><a href="#portfolio" target="_blank"><i class="fa fa-laptop" aria-hidden="true"></i>
+										Portfolio</a></li>
+								<li><a href="#contact" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i> Contact</a>
+								</li>
 							</ul>
 						</div><!-- /.navbar-collapse -->
 					</div><!-- /.container-fluid -->
@@ -84,7 +97,8 @@
 						<div class="col-md-12">
 							<div class="whois">
 								<h1>Whois Harrison?</h1>
-								<p>With over 15 years of experience in web development and marketing, I can help take your next project to the next level. </p>
+								<p>With over 15 years of experience in web development and marketing, I can help take your next
+									project to the next level. </p>
 								<button type="button" class="btn btn-primary">Yes, I'm available for hire</button>
 							</div>
 						</div>
@@ -96,126 +110,131 @@
 			<section class="portfolio">
 				<div class="container">
 					<h3>Portfolio</h3>
-					<div class="websites">
 
 					<p>
 						<a href="https://c1.staticflickr.com/9/8148/29324593462_abebaddc38_k.jpg" data-fancybox="images">
-							<img src="https://c1.staticflickr.com/9/8148/29324593462_f890687b7a_m.jpg" />
+							<img src="https://c1.staticflickr.com/9/8148/29324593462_f890687b7a_m.jpg"/>
 						</a>
 
 						<a href="https://c2.staticflickr.com/6/5499/30972532232_051e1dc57e_h.jpg" data-fancybox="images">
-							<img src="https://c2.staticflickr.com/6/5499/30972532232_e9a298a0c5_m.jpg" />
+							<img src="https://c2.staticflickr.com/6/5499/30972532232_e9a298a0c5_m.jpg"/>
 						</a>
 
 						<a href="https://c1.staticflickr.com/9/8021/29345513551_0c565462d8_k.jpg" data-fancybox="images">
-							<img src="https://c1.staticflickr.com/9/8021/29345513551_16024a89e3_m.jpg" />
+							<img src="https://c1.staticflickr.com/9/8021/29345513551_16024a89e3_m.jpg"/>
 						</a>
 
 						<a href="https://c1.staticflickr.com/9/8148/29324593462_abebaddc38_k.jpg" data-fancybox="images">
-							<img src="https://c1.staticflickr.com/9/8148/29324593462_f890687b7a_m.jpg" />
+							<img src="https://c1.staticflickr.com/9/8148/29324593462_f890687b7a_m.jpg"/>
 						</a>
 
 						<a href="https://c2.staticflickr.com/6/5499/30972532232_051e1dc57e_h.jpg" data-fancybox="images">
-							<img src="https://c2.staticflickr.com/6/5499/30972532232_e9a298a0c5_m.jpg" />
+							<img src="https://c2.staticflickr.com/6/5499/30972532232_e9a298a0c5_m.jpg"/>
 						</a>
 
 						<a href="https://c1.staticflickr.com/9/8021/29345513551_0c565462d8_k.jpg" data-fancybox="images">
-							<img src="https://c1.staticflickr.com/9/8021/29345513551_16024a89e3_m.jpg" />
+							<img src="https://c1.staticflickr.com/9/8021/29345513551_16024a89e3_m.jpg"/>
 						</a>
 
 						<a href="https://c1.staticflickr.com/9/8148/29324593462_abebaddc38_k.jpg" data-fancybox="images">
-							<img src="https://c1.staticflickr.com/9/8148/29324593462_f890687b7a_m.jpg" />
+							<img src="https://c1.staticflickr.com/9/8148/29324593462_f890687b7a_m.jpg"/>
 						</a>
 
 						<a href="https://c2.staticflickr.com/6/5499/30972532232_051e1dc57e_h.jpg" data-fancybox="images">
-							<img src="https://c2.staticflickr.com/6/5499/30972532232_e9a298a0c5_m.jpg" />
+							<img src="https://c2.staticflickr.com/6/5499/30972532232_e9a298a0c5_m.jpg"/>
 						</a>
 
 						<a href="https://c1.staticflickr.com/9/8021/29345513551_0c565462d8_k.jpg" data-fancybox="images">
-							<img src="https://c1.staticflickr.com/9/8021/29345513551_16024a89e3_m.jpg" />
+							<img src="https://c1.staticflickr.com/9/8021/29345513551_16024a89e3_m.jpg"/>
 						</a>
-					</div>
-
-
 					</p>
 				</div>
-			</section>
+		</div>
+		</section>
 
-			<!-- Contact Section -->
-			<section>
-				<div class="container">
-					<div class="row">
-						<h3>Contact</h3>
-						<!--Begin Contact Form-->
-							<form id="contact-form" action="php/mailer.php" method="post">
-								<div class="form-group">
-									<label for="name">Name <span class="text-danger">*</span></label>
-									<div class="input-group">
-										<div class="input-group-addon">
-											<i class="fa fa-user" aria-hidden="true"></i>
-										</div>
-										<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-									</div>
+		<!-- Contact Section -->
+		<section>
+			<div class="container">
+				<div class="row">
+					<h3>Contact</h3>
+					<!--Begin Contact Form-->
+					<form id="contact-form" action="php/mailer.php" method="post">
+						<div class="form-group">
+							<label for="name">Name <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-user" aria-hidden="true"></i>
 								</div>
-								<div class="form-group">
-									<label for="email">Email <span class="text-danger">*</span></label>
-									<div class="input-group">
-										<div class="input-group-addon">
-											<i class="fa fa-envelope" aria-hidden="true"></i>
-										</div>
-										<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="subject">Subject</label>
-									<div class="input-group">
-										<div class="input-group-addon">
-											<i class="fa fa-pencil" aria-hidden="true"></i>
-										</div>
-										<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="message">Message <span class="text-danger">*</span></label>
-									<div class="input-group">
-										<div class="input-group-addon">
-											<i class="fa fa-comment" aria-hidden="true"></i>
-										</div>
-										<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
-									</div>
-								</div>
-						</div>
-
-						<!-- reCAPTCHA -->
-						<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
-
-						<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-						<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-						</form>
-
-						<!--empty area for form error/success output-->
-						<div class="row">
-							<div class="col-xs-12">
-								<div id="output-area"></div>
+								<input type="text" class="form-control" id="name" name="name" placeholder="Name">
 							</div>
 						</div>
-					</div><!--/.row-->
-				</div><!--/.container-->
-			</section>
+						<div class="form-group">
+							<label for="email">Email <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+								</div>
+								<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="subject">Subject</label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-pencil" aria-hidden="true"></i>
+								</div>
+								<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="message">Message <span class="text-danger">*</span></label>
+							<div class="input-group">
+								<div class="input-group-addon">
+									<i class="fa fa-comment" aria-hidden="true"></i>
+								</div>
+								<textarea class="form-control" rows="5" id="message" name="message"
+											 placeholder="Message (2000 characters max)"></textarea>
+							</div>
+						</div>
+				</div>
+
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
+
+				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+				</form>
+
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
+			</div><!--/.row-->
+			</div><!--/.container-->
+		</section>
 
 		<!-- client logos -->
 		<section class="client-logos">
 			<div class="container clients">
 				<div class="row">
-				<ul>
-					<li class="two columns"><img src="images/client-logo-1.png" alt="Animal Humane" class="scale-with-grid"></li>
-					<li class="two columns"><img src="images/client-logo-2.png" alt="Duke Tattoo Fiesta" class="scale-with-grid"></li>
-					<li class="two columns"><img src="images/client-logo-3.png" alt="Robing Waynee" class="scale-with-grid"></li>
-					<li class="two columns"><img src="images/client-logo-4.png" alt="Heritage Hotels" class="scale-with-grid"></li>
-					<li class="two columns"><img src="images/client-logo-5.png" alt="Fitshop" class="scale-with-grid"></li>
-					<li class="two columns"><img src="images/client-logo-7.png" alt="Lorencos" class="scale-with-grid"></li>
-					<li class="two columns"><img src="images/client-logo-8.png" alt="Lobo Club" class="scale-with-grid"></li>
-				</ul>
+					<ul>
+						<li class="two columns"><img src="images/client-logo-1.png" alt="Animal Humane"
+															  class="scale-with-grid"></li>
+						<li class="two columns"><img src="images/client-logo-2.png" alt="Duke Tattoo Fiesta"
+															  class="scale-with-grid"></li>
+						<li class="two columns"><img src="images/client-logo-3.png" alt="Robing Waynee"
+															  class="scale-with-grid"></li>
+						<li class="two columns"><img src="images/client-logo-4.png" alt="Heritage Hotels"
+															  class="scale-with-grid"></li>
+						<li class="two columns"><img src="images/client-logo-5.png" alt="Fitshop" class="scale-with-grid">
+						</li>
+						<li class="two columns"><img src="images/client-logo-7.png" alt="Lorencos" class="scale-with-grid">
+						</li>
+						<li class="two columns"><img src="images/client-logo-8.png" alt="Lobo Club" class="scale-with-grid">
+						</li>
+					</ul>
 				</div>
 			</div>
 		</section>
